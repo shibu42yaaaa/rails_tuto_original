bedd5345889b4ea99ae1672af14570c443fadd26
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-04-01 08:32:18 +0900

    step1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'static_page/home'
+  root to:'static_page#home'
 end
